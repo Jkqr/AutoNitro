eb9cbd409ba3a4b486b05e523c5cf125e28c3a74
Qther <dtaclanlegion@gmail.com>
2020-02-06 20:05:08 +0800

    Update

--- AutoNitro/Program.cs
+++ AutoNitro/Program.cs
@@ -9,14 +9,22 @@ namespace AutoNitro
 {
     class Program
     {
+        public static int attempts;
+        public static int successes;
+        public static string username;
+        public static double balance;
+        
         public static DiscordSocketClient Client { get; private set; }
 
         static void Main(string[] args)
         {
-            Console.Title = "AutoNitro - By iLinked";
+	        Console.Title =
+		        $"AutoNitro - By iLinked | Account: {username} | Hits: {successes}/{attempts} | Balance: ${balance} | Time: {DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss")}";
 
             Config config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("Config.json"));
 
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            
             while (string.IsNullOrEmpty(config.Token))
             {
                 Console.Write("Your Discord token: ");
@@ -29,48 +37,173 @@ namespace AutoNitro
             Client.OnLoggedIn += Client_OnLoggedIn;
             Client.OnMessageReceived += Client_OnMessageReceived;
             Client.Login(config.Token);
-
+            
             Thread.Sleep(-1);
         }
 
         private static void Client_OnMessageReceived(DiscordSocketClient client, MessageEventArgs args)
         {
-            int i = args.Message.Content.IndexOf("discord.gift/");
+	        Console.Title =
+		        $"AutoNitro - By iLinked | Account: {username} | Hits: {successes}/{attempts} | Balance: ${balance} | Time: {DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss")}";
 
-            if (i != -1)
+            string nitroMessage = args.Message.Content;
+            if ((nitroMessage.Contains("discord.gift/") && nitroMessage.Replace("https://discord.gift/", "").Length == 16) ||
+                (nitroMessage.Contains("discordapp.com/gifts/") &&
+                 nitroMessage.Replace("https://discordapp.com/gifts/", "").Length == 24) ||
+                nitroMessage.Contains("discordapp.com/gifts/") || nitroMessage.Contains("discord.gift/"))
             {
-                string gift = args.Message.Content.Substring(i + 13);
-
-                if (gift.Length == 16)
+                string nitroCode;
+                if (nitroMessage.Replace("https://discord.gift/", "").Length == 16)
                 {
-                    try
+                    nitroCode = nitroMessage.Replace("https://discord.gift/", "");
+                }
+                else if (nitroMessage.Replace("discord.gift/", "").Length == 16)
+                {
+                    nitroCode = nitroMessage.Replace("discord.gift/", "");
+                }
+                else if (nitroMessage.Replace("discordapp.com/gifts/", "").Length == 24)
+                {
+                    nitroCode = nitroMessage.Replace("discordapp.com/gifts/", "");
+                }
+                else
+                {
+                    nitroCode = nitroMessage.Replace("https://discordapp.com/gifts/", "");
+                }
+                
+                try
+                {
+                    Client.RedeemNitroGift(nitroCode, args.Message.ChannelId);
+                    string planName = client.GetNitroGift(nitroCode).SubscriptionPlan.Name;
+                    if (planName.Contains("Classic"))
+					{
+						planName = Convert.ToString("$5 Classic");
+						Console.WriteLine(string.Concat(new string[]
+						{
+							"[SUCCESS] Redeemed Nitro Gift: ",
+							nitroCode,
+							" | Server: ",
+							Client.GetGuild(args.Message.GuildId.Value).Name,
+							" | Time: ",
+							DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss"),
+							" | [",
+							planName,
+							"]"
+						}), Console.ForegroundColor = ConsoleColor.Green);
+						successes++;
+						balance += 4.99;
+						Console.Title =
+							$"AutoNitro - By iLinked | Account: {username} | Hits: {successes}/{attempts} | Balance: ${balance} | Time: {DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss")}";;
+					}
+					else if (planName.Contains("Classic") && planName.Contains("Yearly"))
+					{
+						planName = Convert.ToString("$50 Nitro");
+						Console.WriteLine(string.Concat(new string[]
+						{
+							"[SUCCESS] Redeemed Nitro Gift: ",
+							nitroCode,
+							" | Server: ",
+							Client.GetGuild(args.Message.GuildId.Value).Name,
+							" | Time: ",
+							DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss"),
+							" | [",
+							planName,
+							"]"
+						}), Console.ForegroundColor = ConsoleColor.Green);
+						attempts++;
+						balance += 49.99;
+						Console.Title =
+							$"AutoNitro - By iLinked | Account: {username} | Hits: {successes}/{attempts} | Balance: ${balance} | Time: {DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss")}";;
+					}
+					else if (planName == "Nitro Yearly")
+					{
+						planName = Convert.ToString("$100 Nitro");
+						Console.WriteLine(string.Concat(new string[]
+						{
+							"[SUCCESS] Redeemed Nitro Gift: ",
+							nitroCode,
+							" | Server: ",
+							Client.GetGuild(args.Message.GuildId.Value).Name,
+							" | Time: ",
+							DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss"),
+							" | [",
+							planName,
+							"]"
+						}), Console.ForegroundColor = ConsoleColor.Green);
+						successes++;
+						balance += 99.99;
+						Console.Title =
+							$"AutoNitro - By iLinked | Account: {username} | Hits: {successes}/{attempts} | Balance: ${balance} | Time: {DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss")}";;
+					}
+					else if (planName.Contains("Quarterly"))
+					{
+						planName = Convert.ToString("3 Months");
+						Console.WriteLine(string.Concat(new string[]
+						{
+							"[SUCCESS] Redeemed Nitro Gift: ",
+							nitroCode,
+							" | Server: ",
+							Client.GetGuild(args.Message.GuildId.Value).Name,
+							" | Time: ",
+							DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss"),
+							" | [",
+							planName,
+							"]"
+						}), Console.ForegroundColor = ConsoleColor.Green);
+						successes++;
+						balance += 29.97;
+						Console.Title =
+							$"AutoNitro - By iLinked | Account: {username} | Hits: {successes}/{attempts} | Balance: ${balance} | Time: {DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss")}";;
+					}
+					else
+					{
+						planName = Convert.ToString("$10 Nitro");
+						Console.WriteLine(string.Concat(new string[]
+						{
+							"[SUCCESS] Redeemed Nitro Gift: ",
+							nitroCode,
+							" | Server: ",
+							Client.GetGuild(args.Message.GuildId.Value).Name,
+							" | Time: ",
+							DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss"),
+							" | [",
+							planName,
+							"]"
+						}), Console.ForegroundColor = ConsoleColor.Green);
+						successes++;
+						balance += 9.99;
+						Console.Title =
+							$"AutoNitro - By iLinked | Account: {username} | Hits: {successes}/{attempts} | Balance: ${balance} | Time: {DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss")}";;
+					}
+                }
+                catch (DiscordHttpException ex)
+                {
+                    switch (ex.Code)
                     {
-                        Client.RedeemNitroGift(gift, args.Message.ChannelId);
-                    }
-                    catch (DiscordHttpException ex)
-                    {
-                        switch (ex.Code)
-                        {
-                            case DiscordError.NitroGiftRedeemed:
-                                Console.WriteLine("[ERROR] Nitro gift already redeemed: " + gift);
-                                break;
-                            case DiscordError.UnknownGiftCode:
-                                Console.WriteLine("[ERROR] Invalid nitro gift: " + gift);
-                                break;
-                            default:
-                                Console.WriteLine($"[ERROR] Unknown error: {ex.Code} | {ex.ErrorMessage}");
-                                break;
-                        }
+                        case DiscordError.NitroGiftRedeemed:
+                            Console.WriteLine("[ERROR] Nitro gift already redeemed: " + nitroCode);
+                            break;
+                        case DiscordError.UnknownGiftCode:
+                            Console.WriteLine("[ERROR] Invalid Nitro gift: " + nitroCode);
+                            break;
+                        default:
+                            Console.WriteLine($"[ERROR] Unknown error: {ex.Code} | {ex.ErrorMessage}");
+                            break;
                     }
                 }
+
+                attempts++;
+                Console.Title =
+	                $"AutoNitro - By iLinked | Account: {username} | Hits: {successes}/{attempts} | Balance: ${balance} | Time: {DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss")}";
             }
         }
 
         private static void Client_OnLoggedIn(DiscordSocketClient client, LoginEventArgs args)
         {
-            Console.Title = $"AutoNitro - By iLinked | Account: {args.User}";
+            username = args.User.Username;
+            Console.Title =
+	            $"AutoNitro - By iLinked | Account: {username} | Hits: {successes}/{attempts} | Balance: ${balance} | Time: {DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss")}";
 
-            Console.WriteLine($"[SUCCESS] Logged into {args.User}!");
+            Console.WriteLine($"[SUCCESS] Logged into {username}!");
         }
     }
 }
